9729186ee59736b5bddefbaa2900b040b2972753
H301816 <H301816@IE3BLT3BXYNH2>
2019-09-16 16:22:00 +0530

    Code added to convert any language into specific language

--- LanguageHandler/LanguageHandler/ClientSecrets.cs
+++ LanguageHandler/LanguageHandler/ClientSecrets.cs
@@ -2,7 +2,8 @@
 {
    public static class ClientSecrets
     {
-        public static string uri = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0";
+        public static string translateUri = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0";
+        public static string detectUri = "https://api.cognitive.microsofttranslator.com/detect?api-version=3.0";
         public static string key = "3d604e97027840ccb0b4b2b90f2a3163";
     }
 }
--- LanguageHandler/LanguageHandler/LanguageHandler.cs
+++ LanguageHandler/LanguageHandler/LanguageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Dynamic;
 using System.Net.Http;
@@ -10,15 +11,15 @@ namespace LanguageHandler
 {
     public class LanguageHandler
     {
-        public static async Task<string> Translate(string text, string fromLanguage, string toLanguage)
+        public static async Task<string> Translate(string text, string toLanguage)
         {
             dynamic result = string.Empty;
             try
             {
-                string uri = ClientSecrets.uri;
+                string uri = ClientSecrets.translateUri;
                 var uriBuilder = new UriBuilder(uri);
                 var query = HttpUtility.ParseQueryString(uriBuilder.Query);
-                query["from"] = fromLanguage;
+                query["from"] = DetectLanguage(text).Result;
                 query["to"] = toLanguage;
                 uriBuilder.Query = query.ToString();
                 uri = uriBuilder.ToString();
@@ -39,6 +40,44 @@ namespace LanguageHandler
                 }
             }
             catch(Exception ex)
+            {
+                result = new ExpandoObject();
+                result.ErrorCode = 500;
+                result.ErrorMessage = ex.StackTrace.ToString();
+                result = JsonConvert.SerializeObject(result);
+
+            }
+            return result;
+        }
+
+
+        private static async Task<string> DetectLanguage(string text)
+        {
+            dynamic result = string.Empty;
+            try
+            {
+                System.Object[] body = new System.Object[] { new { Text = text } };
+                var requestBody = JsonConvert.SerializeObject(body);
+
+                using (var client = new HttpClient())
+                using (var request = new HttpRequestMessage())
+                {
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(ClientSecrets.detectUri);
+                    request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                    request.Headers.Add("Ocp-Apim-Subscription-Key", ClientSecrets.key);
+
+                    var response = await client.SendAsync(request);
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    result =JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody));
+                    foreach (JObject item in JArray.Parse(result).Children())
+                    {
+                        result = item.SelectToken("language");
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
             {
                 result = new ExpandoObject();
                 result.ErrorCode = 500;
